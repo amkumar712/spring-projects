76d9b99806a5611cb807373dd9b4d50cab0a5a1f
Muthukumar <amkumar712@gmail.com>
2018-07-23 21:10:03 +0530

    First Commit

---
+++ spring-projects/src/main/java/com/myspring/app/SpringProjectsApplication.java
@@ -0,0 +1,12 @@
+package com.myspring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectsApplication.class, args);
+	}
+}
